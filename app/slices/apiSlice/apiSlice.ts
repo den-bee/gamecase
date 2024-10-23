72c9592e8a8b303eac2ec4eb1dcb9e3b08674371
Michiel Baert <michiel.baert@student.ap.be>
2024-10-23 15:27:14 +0200

    fix typo

--- app/slices/apiSlice/apiSlice.ts
+++ app/slices/apiSlice/apiSlice.ts
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import TGame from "../../../types/TGame";
 
-const key = process.env.API_key;
+const key = process.env.API_KEY;
 
 export const rawgApi = createApi({
   reducerPath: "rawgApi",
